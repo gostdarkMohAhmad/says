ba76d105a833dd9c44ecda1b563541cfc0e6f092
gostdarkMohAhmad <44299639+gostdarkMohAhmad@users.noreply.github.com>
2018-11-13 14:12:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', message => {
       
       let args = message.content.split(" ").slice(1);
       let x = args.join(" ")
-        if(message.content.startsWith(prefix + 'ألامر')) {
+        if(message.content.startsWith(prefix + 'say')) {
             message.channel.send(''+x);
                 message.delete(999)
         }
